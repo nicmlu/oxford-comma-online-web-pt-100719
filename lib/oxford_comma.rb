6f6933c65307efe3c46ac974cc6c8b4e31872c43
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-14 23:34:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    array.insert(-2, "and ")
-    array.each_with_index(index) do
-    index[0..-2].join(", ")
+    array.[0..-1].join(", ") << ", and #{array[-1]}"
   end
   else
     array.join(" ")
