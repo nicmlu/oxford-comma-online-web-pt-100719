bdba080d36163d68a191b71ce2b5fd8c48ce0108
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-14 22:54:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    array.insert(-2, "and ")
+    array.join(", ")
+  else
+    array.join(" ")
+  end
 end
