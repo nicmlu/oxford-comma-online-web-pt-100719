f60e67268557b9129f2f250263fafb1c396dbdb9
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-14 23:35:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    array[0..-1].join(", ") << ", and #{array[-1]}"
+    array[0..-2].join(", ") << ", and #{array[-1]}"
   else
     array.join(" ")
   end
